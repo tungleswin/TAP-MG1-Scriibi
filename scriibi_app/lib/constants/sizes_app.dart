dc3ccfe992ccc4694a3e38b8556a9ce316e32df3
Bruce Le <brucele.tt168@gmail.com>
2024-04-05 14:52:36 +1100

    update package structures and create constant sizes and corresponding vertical and horizontal gaps

---
+++ scriibi_app/lib/constants/sizes_app.dart
@@ -0,0 +1,58 @@
+// creating constant numbers for Scriibi app to avoid magic numbers, also
+// for consistency and for the easy of refactoring
+import 'package:flutter/cupertino.dart';
+
+class Sizes {
+  static const size4 = 4.0;
+  static const size8 = 8.0;
+  static const size12 = 12.0;
+  static const size16 = 16.0;
+  static const size20 = 20.0;
+  static const size24 = 24.0;
+  static const size28 = 28.0;
+  static const size32 = 32.0;
+  static const size36 = 36.0;
+  static const size40 = 40.0;
+  static const size44 = 44.0;
+  static const size48 = 48.0;
+  static const size52 = 52.0;
+  static const size56 = 56.0;
+  static const size60 = 60.0;
+  static const size64 = 64.0;
+}
+
+// create corresponding horizontal gap from sizes
+const gapHorizontal4 = SizedBox(width: Sizes.size4);
+const gapHorizontal8 = SizedBox(width: Sizes.size8);
+const gapHorizontal12 = SizedBox(width: Sizes.size12);
+const gapHorizontal16 = SizedBox(width: Sizes.size16);
+const gapHorizontal20 = SizedBox(width: Sizes.size20);
+const gapHorizontal24 = SizedBox(width: Sizes.size24);
+const gapHorizontal28 = SizedBox(width: Sizes.size28);
+const gapHorizontal32 = SizedBox(width: Sizes.size32);
+const gapHorizontal36 = SizedBox(width: Sizes.size36);
+const gapHorizontal40 = SizedBox(width: Sizes.size40);
+const gapHorizontal44 = SizedBox(width: Sizes.size44);
+const gapHorizontal48 = SizedBox(width: Sizes.size48);
+const gapHorizontal52 = SizedBox(width: Sizes.size52);
+const gapHorizontal56 = SizedBox(width: Sizes.size56);
+const gapHorizontal60 = SizedBox(width: Sizes.size60);
+const gapHorizontal64 = SizedBox(width: Sizes.size64);
+
+// create corresponding vertical gap from sizes
+const gapVertical4 = SizedBox(height: Sizes.size4);
+const gapVertical8 = SizedBox(height: Sizes.size8);
+const gapVertical12 = SizedBox(height: Sizes.size12);
+const gapVertical16 = SizedBox(height: Sizes.size16);
+const gapVertical20 = SizedBox(height: Sizes.size20);
+const gapVertical24 = SizedBox(height: Sizes.size24);
+const gapVertical28 = SizedBox(height: Sizes.size28);
+const gapVertical32 = SizedBox(height: Sizes.size32);
+const gapVertical36 = SizedBox(height: Sizes.size36);
+const gapVertical40 = SizedBox(height: Sizes.size40);
+const gapVertical44 = SizedBox(height: Sizes.size44);
+const gapVertical48 = SizedBox(height: Sizes.size48);
+const gapVertical52 = SizedBox(height: Sizes.size52);
+const gapVertical56 = SizedBox(height: Sizes.size56);
+const gapVertical60 = SizedBox(height: Sizes.size60);
+const gapVertical64 = SizedBox(height: Sizes.size64);
