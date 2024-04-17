54bb28fedfbbbdf96aa0f82590a7fbd64c36bb0d
Bruce Le <brucele.tt168@gmail.com>
2024-04-17 16:41:14 +1000

    Fix bugs and add extended part for student card, and camera button

---
+++ scriibi_app/lib/src/features/students_list/widgets/student_card_expanded_part.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:scriibi_app/src/models/student.dart';
+import 'package:scriibi_app/constants/colors_app.dart';
+import 'package:intl/intl.dart';
+
+class StudentExpandedVersionPart extends StatefulWidget {
+  // constructors
+  const StudentExpandedVersionPart({super.key, required this.studentData});
+
+  //variables
+  final Student studentData;
+
+  @override
+  State<StudentExpandedVersionPart> createState() =>
+      _StudentExpandedVersionPartState();
+}
+
+class _StudentExpandedVersionPartState
+    extends State<StudentExpandedVersionPart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 3),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10.0),
+          bottomRight: Radius.circular(10.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colours.appGrey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 7,
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: widget.studentData.fileNames.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String fileName = widget.studentData.fileNames[index];
+                    return Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              // TODO: send delete request to the backend
+                            },
+                            icon: const Icon(
+                              Icons.delete_outline_outlined,
+                              size: 18,
+                              color: Colors.red,
+                            ),
+                          ),
+                          Text(
+                            fileName,
+                            style: TextStyle(color: Colours.appGreen),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                onTap: () {
+                  // TODO: Add camera function here
+                },
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset(
+                    'assets/icons-plus.png',
+                    width: 30,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- scriibi_app/lib/src/features/students_list/widgets/student_card_widget.dart
+++ scriibi_app/lib/src/features/students_list/widgets/student_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scriibi_app/src/models/student.dart';
+import 'package:scriibi_app/src/features/students_list/widgets/student_card_expanded_part.dart';
 
 class StudentCard extends StatefulWidget {
   // constructors
@@ -15,74 +16,93 @@ class StudentCard extends StatefulWidget {
 class _StudentCardState extends State<StudentCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      color: Colors.white,
-      margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(widget.studentData.fullName),
-                      Text(widget.studentData.subtitle),
-                    ],
-                  )),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  alignment: Alignment.center,
-                  child: Text(widget.studentData.className)),
-            ),
-            Expanded(
-              flex: 1,
-              child: Stack(
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: widget.studentData.fileNames
+                  .isNotEmpty // toggle the isExpanded property
+              ? () {
+                  setState(() {
+                    // This toggles the expanded state within setState to trigger a UI update
+                    widget.studentData.isExpanded =
+                        !widget.studentData.isExpanded;
+                  });
+                }
+              : null, // if the fileNames is not empty, do nothing
+          child: Card(
+            elevation: 4,
+            color: Colors.white,
+            margin: const EdgeInsets.all(8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
                 children: [
-                  if (widget.studentData.fileNames.isNotEmpty)
-                    Container(
-                      height: 45,
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${widget.studentData.fileNames.length}', // Number of file uploaded
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green,
-                        ),
-                      ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(widget.studentData.fullName),
+                            Text(widget.studentData.subtitle),
+                          ],
+                        )),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: Text(widget.studentData.className)),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Stack(
+                      children: [
+                        if (widget.studentData.fileNames.isNotEmpty)
+                          Container(
+                            height: 45,
+                            alignment: Alignment.center,
+                            child: Text(
+                              '${widget.studentData.fileNames.length}', // Number of file uploaded
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                        if (widget.studentData.fileNames.isEmpty)
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              'assets/icons-plus.png',
+                              width: 30,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        // The frame for number of documents uploaded
+                        if (widget.studentData.fileNames.isNotEmpty)
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              'assets/icons-document.png',
+                              height: 45,
+                              color: Colors.green,
+                            ),
+                          ),
+                      ],
                     ),
-                  if (widget.studentData.fileNames.isEmpty)
-                    Container(
-                      alignment: Alignment.center,
-                      child: Image.asset(
-                        'assets/icons-plus.png',
-                        width: 30,
-                        color: Colors.amber,
-                      ),
-                    ),
-                  // The frame for number of documents uploaded
-                  if (widget.studentData.fileNames.isNotEmpty)
-                    Container(
-                      alignment: Alignment.center,
-                      child: Image.asset(
-                        'assets/icons-document.png',
-                        height: 45,
-                        color: Colors.green,
-                      ),
-                    ),
+                  ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
-      ),
+        // TODO: show extended part about file names here
+        if (widget.studentData.isExpanded)
+          // StudentExpandedVersionPart(studentData: widget.studentData),
+          StudentExpandedVersionPart(studentData: widget.studentData),
+      ],
     );
-    ;
   }
 }
