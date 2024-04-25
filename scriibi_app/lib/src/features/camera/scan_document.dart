86c0d681d325b423548077073abb406d8e373b40
Bruce Le <brucele.tt168@gmail.com>
2024-04-25 18:26:01 +1000

    Created scanning document feature in the frontend

--- scriibi_app/lib/src/features/camera/scan_document.dart
+++ scriibi_app/lib/src/features/camera/scan_document.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:scriibi_app/src/features/camera/widgets/take_photo_app_bar.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:cunning_document_scanner/cunning_document_scanner.dart';
+import 'package:flutter_document_scanner/flutter_document_scanner.dart';
+import 'package:flutter_genius_scan/flutter_genius_scan.dart';
 
 class ScanDocument extends StatefulWidget {
   const ScanDocument({super.key});
@@ -14,6 +19,8 @@ class _ScanDocumentState extends State<ScanDocument> {
   // variables
   File? file;
   final ImagePicker imagePicker = ImagePicker();
+  List<File> files = [];
+  final pdf = pw.Document();
 
   // main
   @override
@@ -23,9 +30,19 @@ class _ScanDocumentState extends State<ScanDocument> {
         onPressedCamera: getImageFromCamera,
         onPressedGallery: getImageFromGallery,
       ),
-      body: file == null
+      body: files.isEmpty
           ? Center(child: Text('No image selected'))
-          : Image.file(file!),
+          : ListView.builder(
+              itemCount: files.length,
+              itemBuilder: (context, index) {
+                return Image.file(files[index]);
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        // TODO: Add generating pdf function here
+        onPressed: generatePDF,
+        child: Icon(Icons.picture_as_pdf),
+      ),
     );
   }
 
@@ -35,7 +52,8 @@ class _ScanDocumentState extends State<ScanDocument> {
     final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
       if (pickedFile != null) {
-        file = File(pickedFile.path);
+        files.add(File(pickedFile.path));
+        // file = File(pickedFile.path);
       } else {
         print('No image selected.');
       }
@@ -44,15 +62,38 @@ class _ScanDocumentState extends State<ScanDocument> {
 
   // -- To capture image using camera
   Future<void> getImageFromCamera() async {
-    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
+    final scannedFiles = await CunningDocumentScanner.getPictures();
+    ;
     setState(() {
-      if (pickedFile != null) {
-        file = File(pickedFile.path);
+      if (scannedFiles != null && scannedFiles.isNotEmpty) {
+        // If there are scanned files, add each one to the files list
+        for (String path in scannedFiles) {
+          files.add(File(path));
+        }
       } else {
         print('No image captured.');
       }
     });
   }
 
-  // --
+  // -- To generate pdf from chosen images
+  Future<void> generatePDF() async {
+    for (var file in files) {
+      final image = pw.MemoryImage(file.readAsBytesSync());
+      pdf.addPage(
+        pw.Page(
+          build: (context) => pw.Center(
+            child: pw.Padding(
+                padding: pw.EdgeInsets.all(10),
+                child: pw.Image(
+                  image,
+                  fit: pw.BoxFit.contain,
+                )),
+          ),
+        ),
+      );
+    }
+    final pdfFile =
+        await pdf.save(); // handle saving or sharing PDF file as needed
+  }
 }
